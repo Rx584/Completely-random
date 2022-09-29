3a9d01cfda4e1979bb0cfb03ead4fb942cc08f2c
Rx584 <xiao_2010@outlook.com>
2022-09-29 16:19:50 +0800

    更新

--- draw.py
+++ draw.py
@@ -5,11 +5,13 @@ turtle.colormode(255)
 pen = turtle.Pen()
 pen.speed(0)
 tims = int(input('作画次数'))
+
+random.seed(int(input("随机种子")))
 pen.pensize(int(input("粗度")))
 o = input("是否跳过过程？")
 if o == "是":
     turtle.tracer(False)
-random.seed(tims)
+
 for i in range(tims):
     r,g,b = random.randint(1,255),random.randint(1,255),random.randint(1,255)
     pen.forward(random.randint(1, 100))
